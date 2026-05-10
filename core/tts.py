f0be502d571143f99c9f3b7c45cd1d149bc1f07e
Cypher Ghost <90388270+CypherGhost619@users.noreply.github.com>
2026-05-11 00:23:40 +0530

    Update tts.py

--- core/tts.py
+++ core/tts.py
@@ -1,6 +1,9 @@
 from gtts import gTTS
 
-def text_to_speech(text, filename="output.mp3", lang="en"):
-    tts = gTTS(text=text, lang=lang)
-    tts.save(filename)
-    return filename
+def text_to_speech(text, filename, lang="en"):
+    try:
+        tts = gTTS(text=text, lang=lang)
+        tts.save(filename)
+        return filename
+    except:
+        return None
